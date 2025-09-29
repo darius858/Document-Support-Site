4f2614b3dd3d4e885597594f96d05450e8f1c1cd
darius858 <107053838+darius858@users.noreply.github.com>
2025-09-29 14:08:32 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './site/App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./site/App.jsx"; // dit pad klopt met 'src/site/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
